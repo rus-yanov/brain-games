a2d4d8d11a98ecb9c77fac494256f6ba4f08a781
Rustam Akhmedzianov <ahmedzyanov.r.1997@gmail.com>
2022-11-15 10:29:43 +0300

    add gitignore & App

---
+++ app/src/main/java/hexlet/code/App.java
@@ -0,0 +1,19 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+        System.out.println("2 - Even");
+        System.out.println("3 - Calc");
+        System.out.println("4 - GCD");
+        System.out.println("5 - Progression");
+        System.out.println("6 - Prime");
+        System.out.println("0 - Exit");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Your choice: ");
+        int input = sc.nextInt();
+    }
+}
